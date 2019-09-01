96848e59c5bbbe3423992662539ceff85b3c22ac
Peter <prjorgen@gmail.com>
2019-08-31 23:13:02 -0600

    First day -- one code kata down!

---
+++ CodeWars/CodeWars/DirectionsReduction/DirReduction.cs
@@ -0,0 +1,62 @@
+﻿namespace CodeWars.DirectionsReduction
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class DirReduction
+    {
+        static Direction North = new Direction { Value = "NORTH" };
+        static Direction South = new Direction { Value = "SOUTH" };
+        static Direction East = new Direction { Value = "EAST" };
+        static Direction West = new Direction { Value = "WEST" };
+
+        static Direction[] Directions = new[] {North, East, South, West };
+
+        public static string[] dirReduc(string[] directions)
+        {
+            North.Opposite = South;
+            South.Opposite = North;
+            East.Opposite = West;
+            West.Opposite = East;
+
+            var directionsCopy = directions.ToArray();
+            var reducedDirections = new List<string>();
+
+            bool hasOpposite = false;
+            do
+            {
+                hasOpposite = false;
+                reducedDirections.Clear();
+                for (int i = 0; i < directionsCopy.Length; i++)
+                {
+                    bool immediateOpposite = false;
+                    var direction = Directions.First(dir => dir.Value == directionsCopy[i]);
+                    if (i + 1 < directionsCopy.Length)
+                    {
+                        if (directionsCopy[i + 1] == direction.Opposite.Value)
+                        {
+                            i++;
+                            hasOpposite = true;
+                            immediateOpposite = true;
+                        }
+                    }
+
+                    if (!immediateOpposite)
+                    {
+                        reducedDirections.Add(directionsCopy[i]);
+                    }
+                }
+
+                directionsCopy = reducedDirections.ToArray();
+            } while (hasOpposite);
+
+            return reducedDirections.ToArray();
+        }
+
+        private class Direction
+        {
+            public string Value { get; set; }
+            public Direction Opposite { get; set; }
+        }
+    }
+}
--- CodeWars/CodeWars/Program.cs
+++ CodeWars/CodeWars/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace CodeWars
 {
@@ -6,6 +7,18 @@ namespace CodeWars
     {
         static void Main(string[] args)
         {
+            string[] a = new string[] { "NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST" };
+            string[] b = new string[] { "WEST" };
+            string[] result = DirectionsReduction.DirReduction.dirReduc(a);
+
+            Debugger.Break();
+
+            a = new string[] { "NORTH", "WEST", "SOUTH", "EAST" };
+            b = new string[] { "NORTH", "WEST", "SOUTH", "EAST" };
+            result = DirectionsReduction.DirReduction.dirReduc(a);
+
+            Debugger.Break();
+
             Console.WriteLine("This program does nothing.");
         }
     }
