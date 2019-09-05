cd39de7fcb145b36e49702198c808d9b4880ed4d
Peter <prjorgen@gmail.com>
2019-09-04 21:11:45 -0600

    Eliminated the StringBuilder (2106ms)

--- CodeWars/CodeWars/Day4/Kata.cs
+++ CodeWars/CodeWars/Day4/Kata.cs
@@ -1,34 +1,61 @@
 ﻿namespace CodeWars.Day4
 {
-    using System;
-    using System.Text;
+    using System.Collections.Generic;
 
     public class Kata
     {
         public static string PigIt(string str)
         {
-            var result = new StringBuilder();
-
             var words = str.Split(' ');
+            var pigWords = new List<string>();
             foreach (string word in words)
             {
+                string newWord = string.Empty;
+
                 if (char.IsLetter(word[0]))
                 {
                     for (int i = 1; i < word.Length; i++)
                     {
-                        result.Append(word[i]);
+                        newWord += word[i];
                     }
 
-                    result.Append($"{word[0]}ay ");
+                    newWord += $"{word[0]}ay";
                 }
                 else
                 {
-                    result.Append($"{word} ");
+                    newWord = word;
                 }
+
+                pigWords.Add(newWord);
             }
 
-            result.Length -= 1;
-            return result.ToString();
+            return string.Join(" ", pigWords);
         }
+
+        //public static string PigIt(string str)
+        //{
+        //    var result = new StringBuilder();
+
+        //    var words = str.Split(' ');
+        //    foreach (string word in words)
+        //    {
+        //        if (char.IsLetter(word[0]))
+        //        {
+        //            for (int i = 1; i < word.Length; i++)
+        //            {
+        //                result.Append(word[i]);
+        //            }
+
+        //            result.Append($"{word[0]}ay ");
+        //        }
+        //        else
+        //        {
+        //            result.Append($"{word} ");
+        //        }
+        //    }
+
+        //    result.Length -= 1;
+        //    return result.ToString();
+        //}
     }
 }
