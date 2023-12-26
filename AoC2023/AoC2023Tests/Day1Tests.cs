abba74b87a8a65767a673e05bdf8f685dd614d65
Peter Jorgensen <prjorgen@gmail.com>
2023-12-26 14:15:01 -0700

    Part 2 of Day 1

--- AoC2023/AoC2023Tests/Day1Tests.cs
+++ AoC2023/AoC2023Tests/Day1Tests.cs
@@ -30,5 +30,61 @@ namespace AoC2023Tests
                 Debug.WriteLine(result);
             }
         }
+
+        [TestClass]
+        public class Part2ProcessLineTests : Day1Tests
+        {
+            [TestMethod]
+            public void ProcessesFirstAndLastDigitOfLineAsString()
+            {
+                var result = this.classUnderTest.ProcessLinePartTwo("one2three");
+                Assert.AreEqual(13, result);
+            }
+
+            [TestMethod]
+            public void ProcessesExpectedValuesForCalibration()
+            {
+                Dictionary<string, int> expectedValues = new Dictionary<string, int>() { 
+                    { "two1nine", 29 }, 
+                    { "eightwothree", 83 }, 
+                    { "abcone2threexyz", 13 },
+                    { "xtwone3four", 24 },
+                    { "4nineeightseven2", 42 },
+                    { "zoneight234", 14 },
+                    { "7pqrstsixteen", 76} };
+
+                foreach (var row in expectedValues)
+                {
+                    var result = classUnderTest.ProcessLinePartTwo(row.Key);
+                    Assert.AreEqual(row.Value, result);
+                }
+            }
+        }
+
+        [TestClass]
+        public class Part2InputTests : Day1Tests
+        {
+            [TestMethod]
+            public void ProcessInputPartTwo()
+            {
+                var result = this.classUnderTest.ProcessPartTwoInput(null);
+                Assert.IsTrue(result > 0);
+                Debug.WriteLine(result);
+            }
+
+            [TestMethod]
+            public void CalibrationTestPartTwo()
+            {
+                string calibrationValues = @"two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen";
+                var result = this.classUnderTest.ProcessPartTwoInput(calibrationValues);
+                Assert.AreEqual(281, result);
+            }
+        }
     }
 }
--- AoC2023/Day1/Day1.cs
+++ AoC2023/Day1/Day1.cs
@@ -5,10 +5,16 @@
     using System.Linq;
     using System.Reflection;
     using System.Text;
+    using System.Text.RegularExpressions;
     using System.Threading.Tasks;
+    using static System.Runtime.InteropServices.JavaScript.JSType;
 
     public class Day1
     {
+        private static Dictionary<string, char> numberTable = new Dictionary<string, char>{
+        {"zero",'0'},{"one",'1'},{"two",'2'},{"three",'3'},{"four",'4'},{"five",'5'},{"six",'6'},
+        {"seven",'7'},{"eight",'8'},{"nine",'9'} };
+
         public int ProcessPartOneInput()
         {
             string expectedInputFile = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? string.Empty, "Day1", "input.txt");
@@ -25,6 +31,41 @@
                 {
                     var line = reader.ReadLine() ?? string.Empty;
                     var currentValue = ProcessLine(line);
+                    totalValue += currentValue;
+                }
+            }
+
+            return totalValue;
+        }
+
+        public int ProcessPartTwoInput(string? inputOverride)
+        {
+            int totalValue = 0;
+
+            if (inputOverride != null)
+            {
+                var lines = inputOverride.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+                foreach (var line in lines)
+                {
+                    totalValue += ProcessLinePartTwo(line);
+                }
+
+                return totalValue;
+            }
+
+            string expectedInputFile = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? string.Empty, "Day1", "input.txt");
+            if (!File.Exists(expectedInputFile))
+            {
+                throw new Exception("Could not find input file.");
+            }
+
+
+            using (var reader = new StreamReader(expectedInputFile))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine() ?? string.Empty;
+                    var currentValue = ProcessLinePartTwo(line);
                     totalValue += currentValue;
                 }
             }
@@ -71,6 +112,68 @@
             }
 
             char[] chars = { (char)firstDigit, (char)secondDigit }; 
+            return int.Parse(new string(chars));
+        }
+
+        public int ProcessLinePartTwo(string line)
+        {
+            var array = line.ToCharArray();
+
+            int firstDigitIndex = -1;
+            int secondDigitIndex = -1;
+            char? firstDigit = null;
+            char? secondDigit = null;
+
+            for (int i = 0; i <= array.Length - 1; i++)
+            {
+                if (!char.IsNumber(array[i]))
+                {
+                    continue;
+                }
+
+                firstDigit = array[i];
+                firstDigitIndex = i;
+                break;
+            }
+
+            for (int i = array.Length - 1; i >= (firstDigitIndex == -1 ? 0 : firstDigitIndex) ; i--)
+            {
+                if (!char.IsNumber(array[i]))
+                {
+                    continue;
+                }
+
+                secondDigit = array[i];
+                secondDigitIndex = i;
+                break;
+            }
+
+            foreach (var numberString in numberTable.Keys)
+            {
+                if (numberString is null) continue;
+
+                int firstPos = line.IndexOf(numberString, 0);
+                if (firstPos == -1) { continue;  }
+
+                if (line.IndexOf(numberString, 0) < (firstDigitIndex == -1 ? array.Length : firstDigitIndex))
+                {
+                    firstDigitIndex = line.IndexOf(numberString, 0);
+                    firstDigit = numberTable[numberString];
+                }
+
+                if (line.LastIndexOf(numberString, line.Length) > secondDigitIndex)
+                {
+                    secondDigitIndex = line.LastIndexOf(numberString, line.Length);
+                    secondDigit = numberTable[numberString];
+                }
+            }
+
+            if (firstDigit is null || secondDigit is null)
+            {
+                return 0;
+            }
+
+            char[] chars = { (char)firstDigit, (char)secondDigit };
             return int.Parse(new string(chars));
         }
     }
