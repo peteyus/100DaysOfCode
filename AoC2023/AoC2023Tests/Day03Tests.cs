b61e436eb64646d168df3efaaed55a916ef55847
Peter Jorgensen <prjorgen@gmail.com>
2023-12-27 19:19:53 -0700

    Day 3

---
+++ AoC2023/AoC2023Tests/Day03Tests.cs
@@ -0,0 +1,127 @@
+﻿namespace AoC2023Tests
+{
+    using AoC2023.Day3;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    [TestClass]
+    public class Day03Tests
+    {
+        private Day3 classUnderTest = new Day3();
+        string sampleInput = @"467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..";
+
+        [TestClass]
+        public class PartOne : Day03Tests
+        {
+            [TestMethod]
+            public void IdentifiesSymbolsInSampleInput()
+            {
+                var expectedResults = new List<Symbol>() { new Symbol(3, 1), new Symbol(6, 3), new Symbol(3, 4), new Symbol(5, 5), new Symbol(3, 8), new Symbol(5, 8) };
+                var result = classUnderTest.GetSymbolLocations(sampleInput);
+
+                foreach (var expected in expectedResults)
+                {
+                    Assert.IsTrue(result.Any(row => row.X == expected.X && row.Y == expected.Y), $"No match for {expected.X},{expected.Y}");
+                }
+            }
+
+            [TestMethod]
+            public void IdentifiesNumbersInSampleInput()
+            {
+                var expectedResults = new List<GridNumber>()
+                {
+                    new GridNumber() { XStart = 0, XFinish = 2, Y = 0, Value = 467 },
+                    new GridNumber() { XStart = 5, XFinish = 7, Y = 0, Value = 114 },
+                    new GridNumber() { XStart = 2, XFinish = 3, Y = 2, Value = 35 },
+                    new GridNumber() { XStart = 6, XFinish = 8, Y = 2, Value = 633 },
+                    new GridNumber() { XStart = 0, XFinish = 2, Y = 4, Value = 617 },
+                    new GridNumber() { XStart = 7, XFinish = 8, Y = 5, Value = 58 },
+                    new GridNumber() { XStart = 2, XFinish = 4, Y = 6, Value = 592 },
+                    new GridNumber() { XStart = 6, XFinish = 8, Y = 7, Value = 755 },
+                    new GridNumber() { XStart = 1, XFinish = 3, Y = 9, Value = 664 },
+                    new GridNumber() { XStart = 5, XFinish = 7, Y = 9, Value = 598 }
+                };
+
+                var result = classUnderTest.GetGridNumbers(sampleInput);
+
+                foreach (var expected in expectedResults)
+                {
+                    Assert.IsTrue(result.Any(row => row.XStart == expected.XStart && row.XFinish == expected.XFinish && row.Y == expected.Y && row.Value == expected.Value),
+                        $"No match for {expected.Value} at {expected.XStart},{expected.Y}");
+                }
+            }
+
+            [TestMethod]
+            public void IdentifiesNumbersTouchingSymbolsInSampleOutput()
+            {
+                var expectedResults = new List<GridNumber>()
+                {
+                    new GridNumber() { XStart = 0, XFinish = 2, Y = 0, Value = 467 },
+                    new GridNumber() { XStart = 2, XFinish = 3, Y = 2, Value = 35 },
+                    new GridNumber() { XStart = 6, XFinish = 8, Y = 2, Value = 633 },
+                    new GridNumber() { XStart = 0, XFinish = 2, Y = 4, Value = 617 },
+                    new GridNumber() { XStart = 2, XFinish = 4, Y = 6, Value = 592 },
+                    new GridNumber() { XStart = 6, XFinish = 8, Y = 7, Value = 755 },
+                    new GridNumber() { XStart = 1, XFinish = 3, Y = 9, Value = 664 },
+                    new GridNumber() { XStart = 5, XFinish = 7, Y = 9, Value = 598 }
+                };
+
+                var result = classUnderTest.GetGridNumbersTouchingSymbols(classUnderTest.GetSymbolLocations(sampleInput), classUnderTest.GetGridNumbers(sampleInput));
+
+                foreach (var expected in expectedResults)
+                {
+                    Assert.IsTrue(result.Any(row => row.XStart == expected.XStart && row.XFinish == expected.XFinish && row.Y == expected.Y && row.Value == expected.Value),
+                        $"No match for {expected.Value} at {expected.XStart},{expected.Y}");
+                }
+            }
+
+            [TestMethod]
+            public void ParsesSampleInput()
+            {
+                var result = classUnderTest.SumOfPartNumbers(sampleInput);
+                Assert.AreEqual(4361, result);
+            }
+
+            [TestMethod]
+            public void RunPartOne()
+            {
+                var result = classUnderTest.SumOfPartNumbers(null);
+                Debug.WriteLine(result);
+                Assert.AreEqual(540212, result);
+            }
+        }
+
+        [TestClass]
+        public class PartTwo : Day03Tests
+        {
+            [TestMethod]
+            public void GeneratesExpectedOutcomeFromSample()
+            {
+                var result = classUnderTest.SumOfGearRatios(sampleInput);
+                Assert.AreEqual(467835, result);
+            }
+
+            [TestMethod]
+            public void RunPartTwo()
+            {
+                var result = classUnderTest.SumOfGearRatios(null);
+                Debug.WriteLine(result);
+                Assert.AreEqual(87605697, result);
+            }
+        }
+    }
+}
---
+++ AoC2023/Day03/Day3.cs
@@ -0,0 +1,157 @@
+﻿namespace AoC2023.Day3
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Day3
+    {
+        public int SumOfPartNumbers(string? overrideInput)
+        {
+            if (overrideInput == null) overrideInput = ReadInputFromFile();
+            var symbols = GetSymbolLocations(overrideInput);
+            var gridNumbers = GetGridNumbers(overrideInput);
+            var numbersTouchingSymbols = GetGridNumbersTouchingSymbols(symbols, gridNumbers);
+
+            return numbersTouchingSymbols.Sum(number => number.Value);
+        }
+
+        public long SumOfGearRatios(string? overrideInput)
+        {
+            if (overrideInput == null) overrideInput = ReadInputFromFile();
+            var symbols = GetSymbolLocations(overrideInput);
+            var gridNumbers = GetGridNumbers(overrideInput);
+            var gearRatios = GetGearRatios(symbols, gridNumbers);
+            return gearRatios.Sum();
+        }
+
+        public IEnumerable<Symbol> GetSymbolLocations(string input)
+        {
+            var lines = input.Split(Environment.NewLine);
+            var result = new List<Symbol>();
+            for (int y = 0; y < lines.Length; y++)
+            {
+                var line = lines[y];
+                var chars = line.ToCharArray();
+                for (int x = 0; x < chars.Length; x++)
+                {
+                    var c = chars[x];
+                    if (!char.IsNumber(c) && c != '.')
+                    {
+                        result.Add(new Symbol() { X = x, Y = y, Type = c});
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public IEnumerable<GridNumber> GetGridNumbers(string input)
+        {
+            var lines = input.Split(Environment.NewLine);
+            var result = new List<GridNumber>();
+            
+            for (int y = 0; y < lines.Length; y++)
+            {
+                var line = lines[y];
+                var chars = line.ToCharArray();
+                for (int x = 0; x < chars.Length; x++)
+                {
+                    var c = chars[x];
+                    if (char.IsNumber(c))
+                    {
+                        var gridNumber = new GridNumber() { XStart = x, Y = y };
+                        var number = new List<char>();
+                        while (x < chars.Length && char.IsNumber(chars[x]))
+                        {
+                            number.Add(chars[x]);
+                            x++;
+                        }
+                        gridNumber.XFinish = x - 1;
+                        gridNumber.Value = int.Parse(new string(number.ToArray()));
+                        result.Add(gridNumber);
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public IEnumerable<GridNumber> GetGridNumbersTouchingSymbols(IEnumerable<Symbol> symbols, IEnumerable<GridNumber> numbers)
+        {
+            var result = new List<GridNumber>();
+            foreach (var number in numbers)
+            {
+                if (symbols.Any(symbol => symbol.TouchesNumber(number)))
+                {
+                    result.Add(number);
+                    continue;
+                }
+            }
+
+            return result;
+        }
+
+        public IEnumerable<int> GetGearRatios(IEnumerable<Symbol> symbols, IEnumerable<GridNumber> gridNumbers)
+        {
+            var result = new List<int>();
+
+            foreach (var symbol in symbols.Where(s => s.Type == '*'))
+            {
+                int ratio = 1;
+                int touching = 0;
+                foreach (var number in gridNumbers.Where(num => symbol.TouchesNumber(num)))
+                {
+                    touching++;
+                    ratio *= number.Value;
+                }
+
+                if (touching > 1) { result.Add(ratio); }
+            }
+
+            return result;
+        }
+
+        private string ReadInputFromFile()
+        {
+            string expectedInputFile = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? string.Empty, "Day03", "input.txt");
+            if (!File.Exists(expectedInputFile))
+            {
+                throw new Exception("Could not find input file.");
+            }
+
+            using (var reader = new StreamReader(expectedInputFile))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+
+    public class GridNumber
+    {
+        public int XStart { get; set; }
+        public int XFinish { get; set; }
+        public int Y { get; set; }
+        public int Value { get; set; }
+    }
+
+    public class Symbol
+    {
+        public Symbol(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+        public Symbol() {}
+        public int X { get; set; }
+        public int Y { get; set; }
+        public char Type { get; set; }
+        public Boolean TouchesNumber(GridNumber number)
+        {
+            return this.X >= number.XStart - 1 && this.X <= number.XFinish + 1 && number.Y >= this.Y - 1 && number.Y <= this.Y + 1;
+        }
+    }
+}
