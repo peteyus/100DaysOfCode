324c4ac356a9bb00eb8c56e33d063d25e1d0bce7
Peter Jorgensen <prjorgen@gmail.com>
2023-12-29 16:14:41 -0700

    Complete day4

---
+++ AoC2023/AoC2023Tests/Day04Tests.cs
@@ -0,0 +1,63 @@
+﻿namespace AoC2023Tests
+{
+    using AoC2023.Day04;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    [TestClass]
+    public class Day04Tests
+    {
+        private Day4 classUnderTest = new Day4();
+        private string sampleInput = @"Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
+
+        [TestClass]
+        public class PartOne : Day04Tests
+        {
+            [TestMethod]
+            public void ReturnsExpectedValuesForInput()
+            {
+                var result = this.classUnderTest.GetCardValues(sampleInput);
+
+                var sum = result.Sum();
+                Assert.AreEqual(13, sum);
+            }
+
+            [TestMethod]
+            public void RunPartOne()
+            {
+                var result = this.classUnderTest.SumValueOfCards(null);
+                Debug.WriteLine(result);
+                Assert.AreEqual(32001, result);
+            }
+        }
+
+        [TestClass]
+        public class PartTwo : Day04Tests
+        {
+            [TestMethod]
+            public void GeneratesExpectedValuesForSampleInput()
+            {
+                var result = this.classUnderTest.CountAllScratchCards(sampleInput);
+                Assert.AreEqual(30, result);
+            }
+
+            [TestMethod]
+            public void RunPartTwo()
+            {
+                var result = this.classUnderTest.CountAllScratchCards(null);
+                Debug.WriteLine(result);
+                Assert.AreEqual(5037841, result);
+            }
+        }
+    }
+}
---
+++ AoC2023/Day04/Day4.cs
@@ -0,0 +1,131 @@
+﻿namespace AoC2023.Day04
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Day4
+    {
+        private string ReadInputFromFile()
+        {
+            string expectedInputFile = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? string.Empty, "Day04", "input.txt");
+            if (!File.Exists(expectedInputFile))
+            {
+                throw new Exception("Could not find input file.");
+            }
+
+            using (var reader = new StreamReader(expectedInputFile))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+        public int SumValueOfCards(string? overrideInput)
+        {
+            if (overrideInput == null) { overrideInput = this.ReadInputFromFile(); }
+            var values = GetCardValues(overrideInput);
+            return values.Sum();
+        }
+
+        public int CountAllScratchCards(string? overrideInput)
+        {
+            var cards = new List<Card>();
+            for (int i = 1; i <= 223; i++)
+            {
+                cards.Add(new Card { CardId = i });
+            }
+
+            if (overrideInput == null) { overrideInput = this.ReadInputFromFile(); }
+
+            var resultCards = this.GetCardsWithCopiesForWinners(overrideInput, cards);
+
+            return resultCards.Sum(c => c.Copies);
+        }
+
+        public IEnumerable<int> GetCardValues(string input)
+        {
+            var result = new List<int>();
+
+            var cards = input.Replace("  ", " ").Split(Environment.NewLine);
+            foreach (var card in cards)
+            {
+                string cardIdString = card.Split(':')[0].Split(' ')[1];
+                string winningNumbersString = card.Split(':')[1].Split('|')[0];
+                string cardNumbersString = card.Split('|')[1];
+
+                var winningNumbers = winningNumbersString.Trim().Split(' ').Select(s => int.Parse(s));
+                var cardNumbers = cardNumbersString.Trim().Split(' ').Select(s => int.Parse(s));
+                int cardValue = 0;
+                int matches = 0;
+                foreach (var number in cardNumbers)
+                {
+                    if (winningNumbers.Contains(number))
+                    {
+                        matches++;
+                        cardValue = cardValue == 0 ? 1 : cardValue * 2;
+                    }
+                }
+
+                if (cardValue > 0)
+                {
+                    result.Add(cardValue);
+                }
+            }
+
+            return result;
+        }
+
+        public IEnumerable<Card> GetCardsWithCopiesForWinners(string input, IEnumerable<Card> cards)
+        {
+            var result = new List<Card>();
+            var cardsStrings = input.Replace("  ", " ").Split(Environment.NewLine);
+            foreach (var cardString in cardsStrings)
+            {
+                string cardIdString = cardString.Split(':')[0].Split(' ').Last();
+                var card = cards.Single(c => c.CardId == int.Parse(cardIdString));
+                card.Copies++;
+
+                string winningNumbersString = cardString.Split(':')[1].Split('|')[0];
+                string cardNumbersString = cardString.Split('|')[1];
+
+                var winningNumbers = winningNumbersString.Trim().Split(' ').Select(s => int.Parse(s));
+                var cardNumbers = cardNumbersString.Trim().Split(' ').Select(s => int.Parse(s));
+                int cardValue = 0;
+                int matches = 0;
+                foreach (var number in cardNumbers)
+                {
+                    if (winningNumbers.Contains(number))
+                    {
+                        matches++;
+                        cardValue = cardValue == 0 ? 1 : cardValue * 2;
+                    }
+                }
+
+                card.CardPoints = cardValue;
+
+                if (matches > 0)
+                {
+                    for (int i = card.CardId + 1 ; i <= card.CardId + matches; i++)
+                    {
+                        var nextCard = cards.Single(c => c.CardId == i);
+                        nextCard.Copies += card.Copies;
+                    }
+                }
+
+                result.Add(card);
+            }
+
+            return result;
+        }
+    }
+
+    public class Card
+    {
+        public int CardId { get; set; }
+        public int CardPoints { get; set; }
+        public int Copies { get; set; }
+    }
+}
